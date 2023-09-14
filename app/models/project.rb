53c2329e4ebbdcfe802fd5d9262196686ae109df
trndav <farsiteguide@gmail.com>
2023-09-14 18:27:14 +0200

    Add Artifacts for Project. Fix jumbotron image.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
     validates_uniqueness_of :title
     validate :free_plan_can_only_have_one_project
+    has_many :artifacts, dependent: :destroy
 
     def free_plan_can_only_have_one_project
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_103614) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_155051) do
+  create_table "artifacts", force: :cascade do |t|
+    t.string "name"
+    t.string "key"
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_artifacts_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "details"
@@ -31,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_103614) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "artifacts", "projects"
 end
